9b1021fb8e35a367639607c33319b4e64389b89f
mitali shah <mitalishaleenshah@gmail.com>
2020-09-08 11:00:47 -0700

    variables setup

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const setupDiv = document.querySelector("#setup");
+const punchlineDiv = document.querySelector("#punchline");
+const punchlineBtn = document.querySelector("#punchlineBtn");
+const newJokeBtn = document.querySelector("#newJokeBtn");
